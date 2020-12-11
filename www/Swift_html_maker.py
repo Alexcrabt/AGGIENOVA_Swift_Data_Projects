31b38be6126e3e0f7a659d6fb7a9da96b8d908fc
Alexcrabt <74433073+Alexcrabt@users.noreply.github.com>
2020-12-11 14:52:34 -0600

    Relative paths
    Changed the absolute paths to relative paths in the Swift_html_maker code.

--- www/Swift_html_maker.py
+++ www/Swift_html_maker.py
@@ -19,6 +19,7 @@ swift= swift.reset_index(drop=True) #reset the index from 0
 swift = swift.replace(np.nan, '', regex=True) #turn all NANs into '' so it looks nice on webpage
 
 #define all of the columns needed from Swift CSV
+
 snname= swift.SNname 
 host= swift.HostName 
 othersn= swift.OtherName
@@ -28,15 +29,22 @@ red= red.map(lambda x: round(x, 6) if isinstance(x, (int, float)) else x)
 sntype= swift.type
 psn= swift.PSN 
 
-def printsnline(SNnamen, PSNn, SNname2n, galaxyn, redshiftn, typen, n): #creates new HTML code for swift_sn_middle.txt
-#-----------------Paths for the differnt files, you might have to change it a bit for your computer depending on where the files are--------------------------
-    datafilename= os.path.abspath("./AlexandersRepository/data/"+SNnamen+"_uvotB15.1.dat")
-    
-    imagefilename= os.path.abspath("./AlexandersRepository/images/"+SNnamen+"_uvot.png")
 
-    lcfilename= os.path.abspath("./AlexandersRepository/lcplots/"+SNnamen+"_lightcurve.jpg")
+
+
+def printsnline(SNnamen, PSNn, SNname2n, galaxyn, redshiftn, typen, n): #creates new HTML code for swift_sn_middle.txt
+    owd= os.getcwd()
+    os.chdir('..')
+#-----------------data,images, and lcplots must be in current working directory for relative paths to work--------------------------
+
+    data_path= "data/"+SNnamen+"_uvotB15.1.dat"
+    
+    images_path= "images/"+SNnamen+"_uvot.png"
+
+    lcplots_path= "lcplots/"+SNnamen+"_lightcurve.jpg"
     
 #-----------------------------------------------------HTML text---------------------------------------------------------------
+
     texthead= """<tr> \n
     <td align="center" valign="top"> {n} <br></td> \n
              <td align="center" valign="top"><b> {SNnamen} </b><br> &nbsp; \n
@@ -50,14 +58,14 @@ def printsnline(SNnamen, PSNn, SNname2n, galaxyn, redshiftn, typen, n): #creates
                          <td align="center" valign="top"><a \n""".format(**locals())
 
     #imagefile html section:
-    if os.path.isfile(imagefilename)== True:
+    if os.path.exists(images_path)== True:
         textimage= """href="https://github.com/pbrown801/SOUSA/blob/master/images/{SNnamen}_uvot.png"><img \n
                          src="pic.jpg" alt="Swift image" style="border: 0px solid; border: 0px solid; width: 52px; height: 55px;" \n
                           border="0" height="55" width="52"></a><a \n""".format(**locals())
         texthead+= textimage
 
     #lcfilename html section:
-    if os.path.isfile(lcfilename) == True:
+    if os.path.exists(lcplots_path) == True:
         textlc= """href="https://github.com/pbrown801/SOUSA/blob/master/lcplots/{SNnamen}_lightcurve.jpg"><img \n
                     src="light.jpg" alt="Swift UVOT lightcurve" \n
                      style="border: 0px solid; border: 0px solid; width: \n
@@ -65,7 +73,7 @@ def printsnline(SNnamen, PSNn, SNname2n, galaxyn, redshiftn, typen, n): #creates
         texthead+= textlc
                     
     #datafilenmesousa html section:
-    if os.path.isfile(datafilename) == True:
+    if os.path.exists(data_path) == True:
         textdata= """href="https://github.com/pbrown801/SOUSA/blob/master/data/{SNnamen}_uvotB15.1.dat"><img \n
                       src="sousa_galaxy_small.png" alt="Swift UVOT data" \n
                        style="border: 0px solid; border: 0px solid; width: \n
@@ -78,7 +86,7 @@ def printsnline(SNnamen, PSNn, SNname2n, galaxyn, redshiftn, typen, n): #creates
                   </tr>"""
     
     text= texthead + textend
-      
+    os.chdir(owd)
     return(text)
 
 
@@ -99,6 +107,7 @@ bott= codecs.open("swift_sn_bottom.txt",'r')
 html_t= top.read()
 html_m= mid.read()
 html_b= bott.read()
+
 #defines a path for the website HTML combined code
 path = os.path.abspath('SwiftSNWebsite.html')
 url = 'file://' + path
